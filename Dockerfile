da88e049e80f9e4cc29ddb170a1a71249e10e5bc
trueosiris <tim@chaubet.be>
2022-05-19 00:43:23 +0200

    setting up

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
-FROM ghcr.io/linuxserver/baseimage-mono:focal
-FROM cm2network/steamcmd:latest
 USER root
 LABEL maintainer="Tim Chaubet"
 ARG DEBIAN_FRONTEND="noninteractive"
+VOLUME ["/mnt/vrising/server", "/mnt/vrising/persistentdata"]
+
+FROM cm2network/steamcmd:latest AS steamdeploy
+COPY deploy.sh /deploy.sh
+RUN chmod +x /deploy.sh
+RUN deploy.sh
+
+FROM ghcr.io/linuxserver/baseimage-mono:focal
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
-VOLUME ["/mnt/vrising/server", "/mnt/vrising/persistentdata"]
 CMD ["/start.sh"]
--- start.sh
+++ start.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-#if [ -z /mnt/vrising/VRisingServer.exe ]; then
 s=/mnt/vrising/server
 p=/mnt/vrising/persistentdata
-/home/steam/steamcmd/steamcmd.sh +force_install_dir "$s" +login anonymous +app_update 1829350 +quit
-#fi
-echo "steam_appid: "`cat $s/steam_appid.txt`
-echo " "
-if [ ! -f "$p/ServerGameSettings.json" ]; then
-	echo "$p/ServerGameSettings.json not found. Copying default file."
-	cp "$s/VRisingServer_Data/StreamingAssets/Settings/ServerGameSettings.json" "$p/" 2>&1
-fi
-if [ ! -f "$p/ServerHostSettings.json" ]; then
-	echo "$p/ServerHostSettings.json not found. Copying default file."
-	cp "$s/VRisingServer_Data/StreamingAssets/Settings/ServerHostSettings.json" "$p/" 2>&1
-fi
 cd "$s"
 set SteamAppId=`cat $s/steam_appid.txt`
-echo "Starting V Rising Dedicated Server - PRESS CTRL-C to exit"
+echo "Starting mono ..."
+echo "SteamAppId set to $SteamAppId"
+echo " "
 #VRisingServer.exe -persistentDataPath .\save-data -serverName "My V Rising Server" -saveName "world1" -logFile ".\logs\VRisingServer.log"
 
 /usr/bin/tail -f /var/log/dpkg.log
